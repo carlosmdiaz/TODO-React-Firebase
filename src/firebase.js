8dabf424884cefc7a33b0ba0c6a850833e54621b
carlosmdiaz <carlosdiaz3979@gmail.com>
2024-03-17 10:49:33 -0400

    In this commit I added environmental variables.

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhNgRl400MB2JRUFC_mxAQgKRNsLfiBBs",
-  authDomain: "todo-react-tailwind.firebaseapp.com",
-  databaseURL: "https://todo-react-tailwind-default-rtdb.firebaseio.com",
-  projectId: "todo-react-tailwind",
-  storageBucket: "todo-react-tailwind.appspot.com",
-  messagingSenderId: "776120906335",
-  appId: "1:776120906335:web:918d07d71958e3e6bba1ea",
-  measurementId: "G-SHJQTN1F1R"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
